93fc13a3a3a239acef7cd534a0737e7473242e8e
Theopathy <EvanWrynn@gmail.com>
2021-05-24 18:29:35 +0100

    testing herkou

--- app.py
+++ app.py
@@ -12,7 +12,25 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+    
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
 
+    for upload in request.files.getlist("file"):
+        print("LOG: filename: {}".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("LOG: Accept incoming file:", filename)
+        print ("LOG: Save it to:", destination)
+        upload.save(destination)
+
+    return render_template("display_image.html", image_name=filename)
+    
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
